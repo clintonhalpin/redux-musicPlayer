ed6983bad47bd4347b2f03b144a94467b6b0fdab
chalpin <chalpin@salesforce.com>
2015-09-09 17:24:19 -0400

    Some updates

--- src/components/PlayBar.js
+++ src/components/PlayBar.js
@@ -1,9 +1,8 @@
 import React, { Component, PropTypes } from 'react';
 import Radium from 'radium';
-import { PlayButton, Timer, Progress, Icons } from 'react-soundplayer/components';
 import { SoundPlayerContainer } from 'react-soundplayer/addons';
+import { PlayButton, Timer, Progress, Icons } from 'react-soundplayer/components';
 import * as constants from './../flux/constants';
-import soundCloudAudio from 'soundcloud-audio';
 
 @Radium
 export default class Home extends Component {
@@ -13,16 +12,18 @@ export default class Home extends Component {
   }
 
   render() {
-    const { song, playingSong, soundCloudAudio } = this.props;
+    const { song, playingSong } = this.props;
       return (
         <div className="play-bar border-top flex--container">
-          <div className="mr2">
-          </div>
           <div className="flex--1">
             <p className="m0">{ song.title || ' ' }</p>
-            <SoundPlayerContainer streamURL={song.stream_url || ''}  clientId={constants.clientID}>
-                <Progress />
+            
+            <SoundPlayerContainer streamUrl={song.stream_url || ''}  clientId={constants.clientID}>
+                <div className="p1 mb3 mt3 flex flex-center bg-darken-1 red rounded">
+                    <PlayButton {...this.props} />
+                </div>
             </SoundPlayerContainer>
+
           </div>
         </div>
       ); 
